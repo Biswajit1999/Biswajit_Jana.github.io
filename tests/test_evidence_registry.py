5cf547f0cebb6f4d7e08c0af154674ec2bf71d5d
Biswajit Jana <bj7585063100@gmail.com>
2026-09-20 00:51:49 +0100

    Register profile evidence index v1.1.0

--- tests/test_evidence_registry.py
+++ tests/test_evidence_registry.py
@@ -13,8 +13,8 @@ class EvidenceRegistryTests(unittest.TestCase):
     def test_committed_evidence_is_valid(self) -> None:
         document = load_evidence()
         validate_evidence(document)
-        self.assertEqual(document["programme"]["completed"], 7)
-        self.assertEqual(len(document["repositories"]), 7)
+        self.assertEqual(document["programme"]["completed"], 8)
+        self.assertEqual(len(document["repositories"]), 8)
 
     def test_registry_build_is_current_and_accessible(self) -> None:
         generated = render(load_evidence())
